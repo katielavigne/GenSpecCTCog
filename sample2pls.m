1f10e78c21b4646ece5b22c54f9174cd37dc671c
Katie Lavigne <38912376+katielavigne@users.noreply.github.com>
2021-08-09 19:56:19 -0400

    Implement group PLS

--- sample2pls.m
+++ sample2pls.m
@@ -3,90 +3,66 @@
 %                       cortical thickness (X) and cognition/demographics/symptoms (Y)
 % Research Question:    What are the general and specific relationships between cognition and
 %                       cortical thickness in FEP?
-% Analysis Workflow
-%   1. Setup
-%   2. Behaviour PLS analyses covarying for battery
-%       a. Global CT - General Cognition
-%       b. Regional CT - Specific Cognition (covary general cognition)
-%       c. Global CT - General Cognition (covary mean thickness)
-%       d. Regional CT - Specific Cognition (covary both)
-
 % Author: Katie Lavigne (katiemlavigne@gmail.com)
 
 %% SETUP
 
 % Data Preparation: prepare CIVET-processed structural MRI data & glimfile for analysis.
-data = dataprep(); % create data structure (filter: group.FEP)
+data = dataprep(); % create data structure (filter: none)
 
 % Parcellation: create parcellation for vertex data.
 [data.parc] = parcellate(data.Y.smooth20mm, data.avsurf, data.mask, 'dkt', pwd); % parcellate
 
 % Create domain-specific scores
-data.cog = [data.glimfile.zVerbMem, data.glimfile.zVisMem, data.glimfile.zWorkMem, data.glimfile.zProcSpeed, data.glimfile.zExecFunc, data.glimfile.zAtt];
-[data.cog_residmodel, ~, ~, data.cogresid] = ssregress(data.cog, data.glimfile, {'zGCI', 'Pre_Post_MRI'});
+data.cog = [data.glimfile.cog_verb_mem_z, data.glimfile.cog_vis_mem_z, data.glimfile.cog_work_mem_z, data.glimfile.cog_speed_proc_z, data.glimfile.cog_exec_func_z, data.glimfile.cog_vis_att_z];
+data.glimfile.zGCI = mean(data.cog,2);
+[data.cog_residmodel, ~, ~, data.cogresid] = ssregress(data.cog, data.glimfile, {'zGCI', 'Pre_Post_MRI__upgrade_date__20180917_'});
 newvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec'};
 for i = 1:size(newvars,2)
     data.glimfile.(newvars{i}) = data.cogresid(:,i);
 end
 
-save civetsurf_parc.mat data % save output
-
-
-%% PLS - Sample 2
-
-analysis_list = {'GenCT-GenCog', 'GenCT-SpecCog', 'SpecCT-GenCog', 'SpecCT-SpecCog'};
-
-for i = 1:size(analysis_list,2)
-    load civetsurf_parc.mat
-    switch analysis_list{i}
-        case 'GenCT-GenCog'
-            covars = {'Pre_Post_MRI'};
-            behvars = {'zVerbMem', 'zVisMem', 'zWorkMem', 'zProcSpeed', 'zExecFunc', 'zAtt', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-        case 'GenCT-SpecCog'
-            covars = {'Pre_Post_MRI'};
-            behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-        case 'SpecCT-GenCog'
-            covars = {'Pre_Post_MRI', 'meanCorticalMeasure20mm'};
-            behvars = {'zVerbMem', 'zVisMem', 'zWorkMem', 'zProcSpeed', 'zExecFunc', 'zAtt', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-        case 'SpecCT-SpecCog'
-            covars = {'Pre_Post_MRI', 'meanCorticalMeasure20mm'};
-            behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-    end
-    behdesc = {'Verbal Memory', 'Visual Memory', 'Working Memory', 'Processing Speed', 'Executive Function', 'Attention', 'IQ', 'Age', 'Sex', 'Handedness', 'Education', 'SAPS Total', 'SANS Total', 'CPZ_Adherence'};
-    % Regress out covariates
-    [data.residmodel, ~, ~, data.resid] = ssregress(data.parc.ROIs, data.glimfile, covars); 
-    % Run PLS
-    PLS = pls(data.resid, data.parc.pinfo, data.mask, data.avsurf, data.glimfile, behvars, behdesc); % run pls
-    % Save PLS analysis
-    save(fullfile('pls', ['civetsurf_pls_' analysis_list{i} '.mat'])) % save results output
-    movefile('pls', ['pls_' analysis_list{i}]) % rename directory
-    close all % close figures
-end
+data.gfields = fieldnames(data.glimfile);
+save civetsurf_parc_fullsample.mat data % save output
 
 %% VERTEX PLS
 analysis_list = {'GenCT-GenCog', 'GenCT-SpecCog', 'SpecCT-GenCog', 'SpecCT-SpecCog'};
 
 for i = 1:size(analysis_list,2)
-    load civetsurf_parc.mat
-    switch analysis_list{i}
-        case 'GenCT-GenCog'
-            covars = {'Pre_Post_MRI'};
-            behvars = {'zVerbMem', 'zVisMem', 'zWorkMem', 'zProcSpeed', 'zExecFunc', 'zAtt', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-        case 'GenCT-SpecCog'
-            covars = {'Pre_Post_MRI'};
-            behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-        case 'SpecCT-GenCog'
-            covars = {'Pre_Post_MRI', 'meanCorticalMeasure20mm'};
-            behvars = {'zVerbMem', 'zVisMem', 'zWorkMem', 'zProcSpeed', 'zExecFunc', 'zAtt', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-        case 'SpecCT-SpecCog'
-            covars = {'Pre_Post_MRI', 'meanCorticalMeasure20mm'};
-            behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec', 'Full_Scale_IQ', 'Age', 'Gender', 'Handedness', 'Years_of_Education', 'SAPS_Total', 'SANS_Total_No_Att', 'CPZ_Adherence'};
-    end
-    behdesc = {'Verbal Memory', 'Visual Memory', 'Working Memory', 'Processing Speed', 'Executive Function', 'Attention', 'IQ', 'Age', 'Sex', 'Handedness', 'Education', 'SAPS Total', 'SANS Total', 'CPZ_Adherence'};
-    % Regress out covariates
-    [data.residmodel, ~, ~, data.resid] = ssregress(data.Y.smooth20mm, data.glimfile, covars); 
+    load civetsurf_parc_fullsample.mat
+    % Convert Group to categorical
+    data.group = categorical(data.glimfile.group);
+    data.gnames = categories(data.group);
+    % Create group stacked data
+    for g = 1:size(data.gnames,1)
+        switch analysis_list{i}
+            case 'GenCT-GenCog'
+                covars = {'Pre_Post_MRI__upgrade_date__20180917_'};
+                behvars = {'cog_verb_mem_z', 'cog_vis_mem_z', 'cog_work_mem_z', 'cog_speed_proc_z', 'cog_exec_func_z', 'cog_vis_att_z', 'wasi_full_iq_TP1', 'dem_age', 'sex', 'dem_years_educ'};
+%                 behvars = {'cog_verb_mem_z', 'cog_vis_mem_z', 'cog_work_mem_z', 'cog_speed_proc_z', 'cog_exec_func_z', 'cog_vis_att_z'};
+            case 'GenCT-SpecCog'
+                covars = {'Pre_Post_MRI__upgrade_date__20180917_'};
+                behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec', 'wasi_full_iq_TP1', 'dem_age', 'sex', 'dem_years_educ'};
+%                 behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec'};
+            case 'SpecCT-GenCog'
+                covars = {'Pre_Post_MRI__upgrade_date__20180917_', 'meanthickness_20mm_2_1_1'};
+                behvars = {'cog_verb_mem_z', 'cog_vis_mem_z', 'cog_work_mem_z', 'cog_speed_proc_z', 'cog_exec_func_z', 'cog_vis_att_z', 'wasi_full_iq_TP1', 'dem_age', 'sex', 'dem_years_educ'};
+%                 behvars = {'cog_verb_mem_z', 'cog_vis_mem_z', 'cog_work_mem_z', 'cog_speed_proc_z', 'cog_exec_func_z', 'cog_vis_att_z'};
+            case 'SpecCT-SpecCog'
+                covars = {'Pre_Post_MRI__upgrade_date__20180917_', 'meanthickness_20mm_2_1_1'};
+                behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec', 'wasi_full_iq_TP1', 'dem_age', 'sex', 'dem_years_educ'};
+%                 behvars = {'zVerbMem_spec', 'zVisMem_spec', 'zWorkMem_spec', 'zProcSpeed_spec', 'zExecFunc_spec', 'zAtt_spec'};
+        end
+        behdesc = {'Verbal Memory', 'Visual Memory', 'Working Memory', 'Processing Speed', 'Executive Function', 'Attention', 'IQ', 'Age', 'Sex', 'Education'};
+%         behdesc = {'Verbal Memory', 'Visual Memory', 'Working Memory', 'Processing Speed', 'Executive Function', 'Attention'};
+        % Regress out covariates
+        rows = data.group==data.gnames{g};
+        [data.(['residmodel_' data.gnames{g}]), ~, ~, data.(['resid_' data.gnames{g}])] = ssregress(data.Y.smooth20mm(rows,:), data.glimfile(rows,:), covars);
+        data.stacked_data{g,1} = data.(['resid_' data.gnames{g}]);
+    end % for groups
+
     % Run PLS
-    PLS = pls(data.resid, data.parc.pinfo, data.mask, data.avsurf, data.glimfile, behvars, behdesc); % run pls
+    PLS = pls(data.stacked_data, data, behvars, behdesc); % run pls
     % Save PLS analysis
     save(fullfile('pls', ['civetsurf_pls_' analysis_list{i} '.mat'])) % save results output
     movefile('pls', ['pls_vertex_' analysis_list{i}]) % rename directory
